15935104574588688863fe85a06fb0431a8944fe
eduardo diaz <ediazshaw@gmail.com>
2022-07-14 10:57:21 +0200

    input name and get a greeting

--- 01-Ruby/01-Programming-basics/04-What-your-name/lib/interface.rb
+++ 01-Ruby/01-Programming-basics/04-What-your-name/lib/interface.rb
@@ -1,8 +1,14 @@
 require_relative "compute_name"
 
 # TODO: ask for the first name
+puts "Input first name"
+first = gets.chomp
 # TODO: ask for the middle name
+puts "Input middle name"
+middle = gets.chomp
 # TODO: ask for the last name
+puts "Input last name"
+last = gets.chomp
 # TODO: Print a greeting to the user with the full name concatenated,
 #       something like "Hello, Boris Paillard"
-
+puts "Hello, #{compute_name(first, middle, last)}"
