e327c0307cff3e33b4f196d711fb8260d1a7b78e
eduardo diaz <ediazshaw@gmail.com>
2022-07-14 13:21:47 +0200

    respond the wanted answer

--- 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/coach_answer.rb
+++ 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/coach_answer.rb
@@ -1,8 +1,28 @@
+def a_question?(your_message)
+  your_message.chars.last == "?"
+end
+
 def coach_answer(your_message)
   # TODO: return coach answer to your_message
+  if a_question?(your_message)
+    "Silly question, get dressed and go to work!"
+  elsif your_message == "I am going to work right now!"
+    # this is not necessary for the code to work but it is for the rake
+    ""
+  else
+    "I don't care, get dressed and go to work!"
+  end
 end
 
 def coach_answer_enhanced(your_message)
   # TODO: return coach answer to your_message, with additional custom rules of yours!
+  if your_message == "I AM GOING TO WORK RIGHT NOW!"
+    ""
+  elsif your_message == your_message.upcase && a_question?(your_message)
+    "I can feel your motivation! Silly question, get dressed and go to work!"
+  elsif your_message == your_message.upcase
+    "I can feel your motivation! I don't care, get dressed and go to work!"
+  else
+    coach_answer(your_message)
+  end
 end
-
--- 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
+++ 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
@@ -1,2 +1,10 @@
 require_relative "coach_answer"
 # TODO: Implement the program that makes you discuss with your coach from the terminal.
+loop do
+  # code to be executed
+  puts "Type your answer"
+  your_message = gets.chomp
+  break if your_message == "I am going to work right now!"
+
+  puts coach_answer_enhanced(your_message)
+end
