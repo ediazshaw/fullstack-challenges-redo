bfef799c071a0cb1252c99263f5b8dfda005e388
eduardo diaz <ediazshaw@gmail.com>
2022-07-17 19:18:36 +0200

    working for all numbers

--- 01-Ruby/01-Programming-basics/Optional-01-Colorful-Algorithm/lib/colorful.rb
+++ 01-Ruby/01-Programming-basics/Optional-01-Colorful-Algorithm/lib/colorful.rb
@@ -1,4 +1,17 @@
 def colorful?(number)
   # TODO: return true if the number is colorful, false otherwise
+  products_middle = []
+  final_product = 1
+  digits = number.to_s.chars.map do |digit|
+    digit.to_i
+  end
+  digits.each_with_index do |_digit, index|
+    products_middle << (digits[index] * digits[index + 1]) if index != digits.size - 1
+  end
+  digits.each do |digit|
+    final_product = final_product * digit
+  end
+  results = digits + products_middle + final_product.to_s.split
+  results.uniq == results
 end
-
+p colorful?(236)
