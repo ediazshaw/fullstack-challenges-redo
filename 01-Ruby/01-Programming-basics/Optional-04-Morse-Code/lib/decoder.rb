32ef993dce15b6595b7c389d4d82ef856aceaf58
eduardo diaz <ediazshaw@gmail.com>
2022-07-20 09:22:19 +0200

    coder and decoder of morse code

--- 01-Ruby/01-Programming-basics/Optional-04-Morse-Code/lib/decoder.rb
+++ 01-Ruby/01-Programming-basics/Optional-04-Morse-Code/lib/decoder.rb
@@ -1,3 +1,31 @@
+require_relative 'encoder'
 def decode(morse_text)
   # TODO: Decode the morse text you just got!
+  final_array = []
+  sentence = []
+  array_of_arrays = morse_text.split.map do |elem|
+    if elem.include?("|")
+      elem.gsub(/\|/, " | ").split
+    else
+      elem
+    end
+  end
+  array_of_arrays.each do |elem|
+    if elem.instance_of?(Array)
+      elem.each do |subelem|
+        final_array << subelem
+      end
+    else
+      final_array << elem
+    end
+  end
+  final_array.each do |c|
+    if c == "|"
+      sentence << " "
+    else
+      sentence << MORSE_CODE.key(c).to_s.upcase
+    end
+  end
+  sentence.join
 end
+p decode(".... ..|--. ..- -.-- ...")
--- 01-Ruby/01-Programming-basics/Optional-04-Morse-Code/lib/encoder.rb
+++ 01-Ruby/01-Programming-basics/Optional-04-Morse-Code/lib/encoder.rb
@@ -1,4 +1,23 @@
+MORSE_CODE = {
+  a: '.-', b: '-...', c: '-.-.', d: '-..', e: '.', f: '..-.',
+  g: '--.', h: '....', i: '..', j: '.---', k: '-.-', l: '.-..',
+  m: '--', n: '-.', o: '---', p: '.--.', q: '--.-', r: '.-.', s: '...',
+  t: '-', u: '..-', v: '...-', w: '.--', x: '-..-', y: '-.--', z: '--..'
+}
+
 def encode(text)
   # TODO: write a method which returns the morse sentence for the given text
   # NOTE: you may want to extract a `encode_word` method
+  encoded = []
+  text.downcase.chars.each do |c|
+    if c == " "
+      encoded << "|"
+    elsif c.match?(/[A-Za-z]/)
+      encoded << MORSE_CODE[c.to_sym]
+    end
+  end
+  almost_morse = encoded.join(" ")
+  almost_morse.gsub(/\s\|\s/, "|")
 end
+
+# p encode("hello, world'")
